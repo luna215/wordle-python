ea6abbd45c28b4ce753ed292806b840f22198723
Paul Luna <paulluna0215@gmail.com>
2023-03-31 13:57:54 -0700

    added poetry

--- wyrdle.py
+++ wyrdle.py
@@ -1,52 +1,73 @@
 import pathlib
 import random
 from string import ascii_letters
+from rich.console import Console
 
-WORD_LIST = pathlib.Path("wordlist.txt")
 WORD_LIMIT = 5
 
-words = [
-    word.upper()
-    for word in WORD_LIST.read_text(encoding="utf-8").split("\n")
-    if (word) == WORD_LIMIT and all(letter in ascii_letters for letter in word)
-]
-secret_word = random.choice(words)
+def get_random_word(word_list):
+    """Get a random five-letter word from a list of strings.
 
-for guess_num in range(1, 7):
-    guess = input(f"\nGuess {guess_num}: ").upper()
+    ## Example:
 
-    if guess == secret_word:
-        print("Correct")
-        break
+    >>> get_random_word(["snake", "worm", "it'll"])
+    'SNAKE'
+    """
+    words = [
+        word.upper()
+        for word in word_list
+        if (word) == WORD_LIMIT and all(letter in ascii_letters for letter in word)
+    ]
+    return random.choice(words)
 
+def show_guess(guess, word):
+    """Show the user's guess on the terminal and classify all letters.
 
+    ## Example:
+
+    >>> show_guess("CRANE", "SNAKE")
+    Correct letters: A, E
+    Misplaced letters: N
+    Wrong letters: C, R
+    """
     correct_letters = {
-        letter for letter, correct in zip(guess, secret_word) if letter == correct
+        letter for letter, correct in zip(guess, word) if letter == correct
     }
-    misplaced_letters = set(guess) & set(secret_word) - correct_letters
-    wrong_letters = set(guess) - set(secret_word)
+    misplaced_letters = set(guess) & set(word) - correct_letters
+    wrong_letters = set(guess) - set(word)
 
-    print("Correct letters:", ",".join(sorted(correct_letters)))
-    print("Misplaced letters:", ",".join(sorted(misplaced_letters)))
-    print("Wrong letters", ",".join(sorted(wrong_letters)))
+    print("Correct letters:", ", ".join(sorted(correct_letters)))
+    print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
+    print("Wrong letters:", ", ".join(sorted(wrong_letters)))
 
-else:
-    print(f"The word was {secret_word}")
+
+def game_over(word):
+    print(f"The word was {word}")
+
+
+def refresh_page(headline):
+    console.clear()
+    console.rule(f"[bold blue]:leafy_green: {headline} :leafy_green:[/]\n")
 
 
 def main():
 
     # Pre-process
-    word = get_random_word(...)
+    words_path = pathlib.Path(__file__).parent / "wordlist.txt"
+    secret_word = get_random_word(words_path.read_text(encoding="utf-8").split("\n"))
 
     # Process (main loop)
     for guess_num in range(1, 7):
         guess = input(f"\nGuess {guess_num}: ").upper()
 
-        show_guess(...)
-        if guess == word:
+        show_guess(guess, secret_word)
+        if guess == secret_word:
             break
     
     # Post-process
     else:
         game_over(...)
+
+
+if __name__ == "__main__":
+    main()
