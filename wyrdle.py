2911298220c1309c3d9fac47921ceb480193b77a
Paul Luna <paulluna0215@gmail.com>
2023-03-31 13:41:44 -0700

    added template for reorganizing code

--- wyrdle.py
+++ wyrdle.py
@@ -1,11 +1,14 @@
 import pathlib
 import random
+from string import ascii_letters
 
 WORD_LIST = pathlib.Path("wordlist.txt")
+WORD_LIMIT = 5
 
 words = [
     word.upper()
-    for word in WORD_LIST.read_text(encoding="utf-8").strip().split("\n")
+    for word in WORD_LIST.read_text(encoding="utf-8").split("\n")
+    if (word) == WORD_LIMIT and all(letter in ascii_letters for letter in word)
 ]
 secret_word = random.choice(words)
 
@@ -29,3 +32,21 @@ for guess_num in range(1, 7):
 
 else:
     print(f"The word was {secret_word}")
+
+
+def main():
+
+    # Pre-process
+    word = get_random_word(...)
+
+    # Process (main loop)
+    for guess_num in range(1, 7):
+        guess = input(f"\nGuess {guess_num}: ").upper()
+
+        show_guess(...)
+        if guess == word:
+            break
+    
+    # Post-process
+    else:
+        game_over(...)
